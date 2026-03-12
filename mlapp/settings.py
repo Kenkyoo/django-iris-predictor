5002e1295c9aeef4e20f11a7939eccdb254fe8da
Franco <frfantusatti@protonmail.com>
2026-03-12 10:52:20 -0300

    add predictor model

--- mlapp/settings.py
+++ mlapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fqgse=#+a+x*=4bb47_ymvpm$h+&v+8c$-%fh0hdbeyrq#9f!8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "predictor",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mlapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
