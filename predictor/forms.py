004e72f93bb505f8e36ace08601043aa1996e6d4
Franco <frfantusatti@protonmail.com>
2026-03-13 10:59:00 -0300

    ui, ux design

--- predictor/forms.py
+++ predictor/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 
 class IrisForm(forms.Form):
-    sepal_length = forms.FloatField(min_value=0, label="Sepal length (cm)")
-    sepal_width  = forms.FloatField(min_value=0, label="Sepal width (cm)")
-    petal_length = forms.FloatField(min_value=0, label="Petal length (cm)")
-    petal_width  = forms.FloatField(min_value=0, label="Petal width (cm)")
+    sepal_length = forms.FloatField(widget=forms.NumberInput(attrs={"class": "input", "placeholder": "e.g. 5.1"}))
+    sepal_width  = forms.FloatField(widget=forms.NumberInput(attrs={"class": "input", "placeholder": "e.g. 3.5"}))
+    petal_length = forms.FloatField(widget=forms.NumberInput(attrs={"class": "input", "placeholder": "e.g. 1.4"}))
+    petal_width  = forms.FloatField(widget=forms.NumberInput(attrs={"class": "input", "placeholder": "e.g. 0.2"}))
